dc64b28a8d4d41de8681f13ce724b9923b912e3d
João Cabeleira <cabeleira_31@hotmail.com>
2022-04-13 15:14:32 -0300

    about phrase changes

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -8,18 +8,12 @@ const About = () => {
             <div className='about-description'>
                 <h3>Sobre mim:</h3>
                 <p>Me chamo João Pedro, sou apaixonado por desenvolvimento
-                    e por resolver problemas. A cobrança e novos desafios são
+                    e por resolver problemas. Metas e novos desafios são
                     o que me motivam a ir mais longe.</p>
                 <h4>Nome:</h4>
                 <p>João Pedro Parcianello Cabeleira</p>
                 <h4>Idade:</h4>
                 <p>29 anos</p>
-                <h4>Nacionalidade:</h4>
-                <p>Brasileiro</p>
-                <h4>Idiomas:</h4>
-                <p>Português, Inglês e Espanhol.</p>
-                <h4>Localização:</h4>
-                <p>São Borja - RS</p>
             </div>
             <div className='about-img'>
                 <img src={aboutimg} alt="about" />
